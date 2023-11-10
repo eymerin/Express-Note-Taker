abffbd6271b6a76fb685f379771eff053116368f
eymerin <garrett.bryce.young@gmail.com>
2023-11-09 20:30:18 -0700

    added uniqid

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const PORT = process.env.PORT || 3001;
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
-const db = require('./db/db.json');
+var {notesAll} = require('./db/db.json');
 
 //create app object
 const app = express();
@@ -20,68 +20,39 @@ app.get('/notes', (req,res) => {
 });
 
 app.get('/api/notes', (req,res) =>{
-    res.json(db);
+    res.json(notesAll);
 });
 
-function addNote(body, notesAll) {
-    const newNote = body;
-    if (!Array.isArray(notesAll))
-    notesAll = [];
-    
-    if (notesAll.length === 0)
-    notesAll.push(0);
-
-    body.id = notesAll[0];
-    notesAll[0]++;
+//Post note
+app.post('/api/notes', (req,res) =>{
+    const newNote = req.body;
+    newNote.id = uniqid();
 
     notesAll.push(newNote);
     fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
-        JSON.stringify(notesAll, null, 2)
+        JSON.stringify({notesAll}, null, 2)
     );
-    return newNote;
-}
 
-app.post('/api/notes', (req, res) => {
-    const newNote = addNote(req.body, allNotes);
     res.json(newNote);
 });
 
-function deleteNote(id, notesAll) {
-    for (let i = 0; i < notesAll.length; i++) {
-        let note = notesAll[i];
+//Delete note
+app.delete('/api/notes/:id', (req,res) => {
+    let id = req.params.id;
+    notesAll = notesAll.filter(function(note) { return note.id != id})
 
-        if (note.id == id) {
-            notesAll.splice(i, 1);
-            fs.writeFileSync(
-                path.join(__dirname, './db/db.json'),
-                JSON.stringify(notesAll, null, 2)
-            );
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({notesAll}, null, 2)
+    );
 
-            break;
-        }
+    if(notesAll){
+        res.json("Database Found");
     }
-}
-
-function deleteNote(id, notesAll) {
-    for (let i = 0; i < notesAll.length; i++) {
-        let note = notesAnotesAllrray[i];
-
-        if (note.id == id) {
-            notesAll.splice(i, 1);
-            fs.writeFileSync(
-                path.join(__dirname, './db/db.json'),
-                JSON.stringify(notesAll, null, 2)
-            );
-
-            break;
-        }
+    else{
+        res.json(404);
     }
-}
-
-app.delete('/api/notes/:id', (req, res) => {
-    deleteNote(req.params.id, notesAll);
-    res.json(true);
 });
 
 //App PORT
